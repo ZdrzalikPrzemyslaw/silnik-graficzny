ffdbb9c9797ec1434b941f8c7ef59c2400ba4329
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-03-03 16:59:49 +0100

    valuable comment

--- Structures/Plane.cs
+++ Structures/Plane.cs
@@ -13,6 +13,8 @@ public class Plane
 
     public Plane(Vector3 inNormal, Vector3 point)
     {
+        // TODO: w unity ten dystans jest dodatni lub ujemny w zaleznosci od tego jak skierowany jest wektor normalny,
+        //      u nas jest narazie zawsze dodatni idk czy tak to ma zostac?
         distance = point.Magnitude();
         normal = inNormal;
     }
