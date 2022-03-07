20b60cd5236bce655582c7ff289845a40c18d5d3
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-07 10:47:12 +0100

    Add tests

--- Zadanie1/UnitTest1.cs
+++ Zadanie1/UnitTest1.cs
@@ -20,9 +20,25 @@ public class UnitTest1
         
         List<Vector3> x1 = S.Intersection(R1);
         Assert.AreEqual(2, x1.Count);
+        Assert.AreEqual(new Vector3(0, 0, -10), x1[0]);
+        Assert.AreEqual(new Vector3(0, 0, 10), x1[1]);
+        
         List<Vector3> x2 = S.Intersection(R2);
         Assert.AreEqual(0, x2.Count);
+        
         List<Vector3> x3 = S.Intersection(R3);
         Assert.AreEqual(1, x3.Count);
+        Assert.AreEqual(new Vector3(10, 0, 0), x3[0]);
+    }
+
+    [TestMethod]
+    public void TestPlaneIntersection()
+    {
+        Plane P = new Plane(new Vector3(0, 0.5, 0.5), new Vector3(0, 0, 0));
+        Ray R2 = new Ray(new Vector3(0, 0, -20),
+            new Vector3(new Vector3(0, 1, 0)));
+        new Ray(Vector3.Zero(), P.Normal).PointAtDistanceFromOrigin(P.Distance);
+        
+        Assert.AreEqual(new Vector3(0, 20, -20), P.Intersection(R2));
     }
 }
