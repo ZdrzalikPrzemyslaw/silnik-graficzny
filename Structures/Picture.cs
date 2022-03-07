9e61533944c9f343440b7d4852e7006375e6ab93
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-07 18:57:32 +0100

    Add Bitmap

---
+++ Structures/Picture.cs
@@ -0,0 +1,22 @@
+﻿using SkiaSharp;
+
+namespace Structures;
+
+public class Picture
+{
+    public SKBitmap Bitmap { get; set; }
+
+
+    public Picture(int xSize, int ySize)
+    {
+        Bitmap = new SKBitmap(xSize, ySize);
+    }
+
+    public void SetPixel(int x, int y, LightIntensity pixel)
+    {
+        Bitmap.SetPixel(x, y, new SKColor(
+            Convert.ToByte(pixel.R * 255),
+            Convert.ToByte(pixel.G * 255),
+            Convert.ToByte(pixel.B * 255)));
+    }
+}
