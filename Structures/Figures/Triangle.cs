afa4207d88990d93dda14af0333276c54f085f71
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-16 12:41:03 +0100

    Add triangle

---
+++ Structures/Figures/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using Structures.MathObjects;
+using Structures.Render;
+
+namespace Structures.Figures;
+
+public class Triangle : Figure
+{
+    public Vector3 A { get; set; } = Vector3.Zero();
+    private Plane _plane;
+    public Vector3 B { get; set; } = Vector3.Zero();
+    public Vector3 C { get; set; } = Vector3.Zero();
+
+
+    public Triangle(Vector3 inNormal, Vector3 point, LightIntensity lightIntensity, Vector3 a,
+        Vector3 b, Vector3 c)
+    {
+        _plane = new Plane(inNormal, point, lightIntensity);
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public override bool Equals(Figure? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Intersects(Ray ray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Vector3? Intersection(Ray ray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override List<Vector3> Intersections(Ray ray)
+    {
+        throw new NotImplementedException();
+    }
+}
