5bdbbd37dad342dca9897a888cae2f8ec7f39975
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-18 11:20:21 +0100

    Add point light and directional light

---
+++ Structures/Render/Light/DirectionalLightSource.cs
@@ -0,0 +1,33 @@
+﻿using Structures.Figures;
+using Structures.MathObjects;
+
+namespace Structures.Render.Light;
+
+public class DirectionalLightSource : LightSource
+{
+    public Vector3 Direction { get; set; }
+
+    public DirectionalLightSource(Vector3 direction)
+    {
+        Direction = direction.GetNormalized();
+    }
+
+    public DirectionalLightSource() : this(Vector3.Down())
+    {
+    }
+
+    public override Vector3 GetDiffuse(Vector3 cameraPosition, PointOfIntersection pointOfIntersection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Vector3 GetSpecular(Vector3 cameraPosition, PointOfIntersection pointOfIntersection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsInShadow(PointOfIntersection pointOfIntersection, Scene scene)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Structures/Render/Light/PointLightSource.cs
@@ -0,0 +1,24 @@
+﻿using Structures.Figures;
+using Structures.MathObjects;
+
+namespace Structures.Render.Light;
+
+public class PointLightSource : LightSource
+{
+    public Vector3 location { get; set; }
+    
+    public override Vector3 GetDiffuse(Vector3 cameraPosition, PointOfIntersection pointOfIntersection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Vector3 GetSpecular(Vector3 cameraPosition, PointOfIntersection pointOfIntersection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsInShadow(PointOfIntersection pointOfIntersection, Scene scene)
+    {
+        throw new NotImplementedException();
+    }
+}
