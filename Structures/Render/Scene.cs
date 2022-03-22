9cefd9dd414252d6e1b89afb7f823c5937d87325
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-22 16:17:38 +0100

    Add light sources list

--- Structures/Render/Scene.cs
+++ Structures/Render/Scene.cs
@@ -1,11 +1,13 @@
 ﻿using Structures.Figures;
 using Structures.MathObjects;
+using Structures.Render.Light;
 
 namespace Structures.Render;
 
 public class Scene : AbstractFigureList<ComplexFigure>
 {
     private readonly List<ComplexFigure> _figures = new();
+    private readonly List<LightSource> _lightSources = new();
 
     public Scene()
     {
@@ -16,14 +18,34 @@ public class Scene : AbstractFigureList<ComplexFigure>
     {
     }
 
+    public Scene(List<SimpleFigure?> simpleFigures, List<LightSource?> lightSources) : this(simpleFigures
+        .Where(i => i is not null).Cast<SimpleFigure>()
+        .ToArray(), lightSources
+        .Where(i => i is not null).Cast<LightSource>()
+        .ToArray())
+    {
+    }
+
     public Scene(params SimpleFigure[] figures)
     {
         _figures.Add(new ComplexFigure(figures));
     }
 
+    public Scene(SimpleFigure[] figures, LightSource[] lightSources)
+    {
+        _figures.Add(new ComplexFigure(figures));
+        _lightSources.AddRange(lightSources);
+    }
+
     public Scene(params ComplexFigure[] figures)
     {
         _figures.AddRange(figures);
+    }
+
+    public Scene(ComplexFigure[] figures, LightSource[] lightSources)
+    {
+        _figures.AddRange(figures);
+        _lightSources.AddRange(lightSources);
     }
 
     public Scene(ComplexFigure figure)
