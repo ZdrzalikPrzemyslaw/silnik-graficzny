34e9ceb1a496a7d59eb3abb2db462a244269e2e0
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-03-27 11:33:11 +0200

    Add one more point light to scene

--- Zad3/Zad3.cs
+++ Zad3/Zad3.cs
@@ -43,15 +43,15 @@ internal class Zad3
                 new Vector3(2, -3, 4), 2)
         );
         // scene.AddFigure(new OBJFileParser().ParseFile(new Vector3(0, 0, 0), "./cube.obj"));
-        scene.AddLight(new AmbientLightSource(new LightIntensity(0.2, 0.2, 0.2)));
+        scene.AddLight(new AmbientLightSource(new LightIntensity(0.3, 0.3, 0.3)));
         // scene.AddLight(new PointLightSource(new LightIntensity(0.2, 1, 0.8), new Vector3(-5, -5, 5), 1, 1));
         // scene.AddLight(new PointLightSource(new LightIntensity(0.8, 0.2, 0.2), new Vector3(0, -10, 0), 1, 1));
         // scene.AddLight(new PointLightSource(new LightIntensity(0.8, 0.2, 0.4), new Vector3(0, 0, -10), 1, 1));
         var pointLight = new PointLightSource(new LightIntensity(0.5, 0.75, 0.83), new Vector3(-4, 5, 9), 1, 1);
-        // var pointLight2 = new PointLightSource(new LightIntensity(0.8, 0.1, 0.77), new Vector3(4, 5, 9), 1, 1);
+        var pointLight2 = new PointLightSource(new LightIntensity(0.8, 0.1, 0.77), new Vector3(4, 5, 9), 1, 1);
         // var plane = new Plane(Vector3.Down(), 10);
         scene.AddLight(pointLight);
-        // scene.AddLight(pointLight2);
+        scene.AddLight(pointLight2);
         // scene.AddLight(new SurfaceLightSource(pointLight, plane, Vector3.Back(), 2, 2, 4, 4).GetLightSources());
 
         var picture1 = cameraOrth.RenderScene(scene);
